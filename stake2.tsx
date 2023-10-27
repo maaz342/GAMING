f475f6093b70d35c143442315ea82c1720f92165
maaz342 <114800104+maaz342@users.noreply.github.com>
2023-10-27 16:57:31 +0500

    Add files via upload

---
+++ stake2.tsx
@@ -0,0 +1,64 @@
+"use client"
+import React, { useState } from 'react';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+const Home = () => {
+<Navbar/>
+  return (
+    <div>
+    <div className='h-32 w-auto border border-gray-500 mr-96'></div>
+   
+
+    
+<div className="h-16 mt-12 w-auto border-8 border-red-500 mr-96"><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero atque est officiis minus laudantium, dolore, velit officia enim harum ex reiciendis eum dignissimos expedita, deleniti non cupiditate minima explicabo nesciunt?</p>
+    <div className="flex overflow-x-auto border space-x-10">
+      <div className="h-12 w-96 border-8 border-red-500">Tab 1</div>
+      <div className="h-12 w-96 border-8 border-red-500">Tab 2</div>
+      <div className=" h-12 border-8 border-red-500"><button>SAMPLE</button></div>
+    
+    </div>
+    <div className="flex overflow-x-auto  border space-x-10">
+      <div className="h-12 w-96 border-8 border-red-500">Tab 1</div>
+      <div className="h-12 w-96 border-8 border-red-500">Tab 2</div>
+      <div className="h-12 border-8 border-red-500"><button>SAMPLE</button></div>
+    </div>
+
+    </div>
+    <div className='h-32 mt-24 w-auto border border-gray-500 mr-96'></div>
+    <div className="h-16 mt-12 w-auto border-8 border-red-500 mr-96"><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero atque est officiis minus laudantium, dolore, velit officia enim harum ex reiciendis eum dignissimos expedita, deleniti non cupiditate minima explicabo nesciunt?</p>
+    <div className="flex overflow-x-auto border space-x-10">
+      <div className="h-12 w-96 border-8 border-red-500">Tab 1</div>
+      <div className="h-12 w-96 border-8 border-red-500">Tab 2</div>
+      <div className=" h-12 border-8 border-red-500"><button>SAMPLE</button></div>
+    
+    </div>
+    <div className="flex overflow-x-auto  border space-x-10">
+      <div className="h-12 w-96 border-8 border-red-500">Tab 1</div>
+      <div className="h-12 w-96 border-8 border-red-500">Tab 2</div>
+      <div className="h-12 border-8 border-red-500"><button>SAMPLE</button></div>
+    </div>
+
+    </div>
+    <div className="h-16 mt-24 w-auto border-8 border-red-500 mr-96"><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero atque est officiis minus laudantium, dolore, velit officia enim harum ex reiciendis eum dignissimos expedita, deleniti non cupiditate minima explicabo nesciunt?</p>
+    <div className="flex overflow-x-auto border space-x-10">
+      <div className="h-12 w-96 border-8 border-red-500">Tab 1</div>
+      <div className="h-12 w-96 border-8 border-red-500">Tab 2</div>
+      <div className=" h-12 border-8 border-red-500"><button>SAMPLE</button></div>
+    
+    </div>
+    <div className="flex overflow-x-auto  border space-x-10">
+      <div className="h-12 w-96 border-8 border-red-500">Tab 1</div>
+      <div className="h-12 w-96 border-8 border-red-500">Tab 2</div>
+      <div className="h-12 border-8 border-red-500"><button>SAMPLE</button></div>
+    </div>
+
+    </div>
+    <Footer />
+
+    </div>
+
+  );
+};
+
+export default Home;
